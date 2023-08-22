cc906d6a5101dd7b5069de4878708f199f192a19
Justin Jung <justin@bustinbung.com>
2023-08-22 00:31:06 -0400

    Add update message

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,3 +6,4 @@
 
 <h2 class="center">justin jung // a student of media and music</h2><br>
 <p class="center">hi there, and welcome to my site!</p>
+<p class="center">currently working on (yet another) rewrite, this time in a static web framework, <a href="https://astro.build" target="_blank">Astro</a>. check out nthe progress <a href="https://github.com/bustinbung/bustinbung.com/tree/astro-dev" target="_blank">here</a>. v5.0 is on the way!</p>
