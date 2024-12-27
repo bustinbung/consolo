3983fc62a9c3807f4d7906c3f9935c70fa3ce021
Justin Jung <justin@bustinbung.com>
2024-12-27 01:53:40 -0500

    feat: change default code theme

--- astro.config.ts
+++ astro.config.ts
@@ -3,6 +3,11 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: "https://bustinbung.com",
+    markdown: {
+        shikiConfig: {
+            theme: 'catppuccin-frappe',
+        }
+    },
     vite: {
         css: {
             preprocessorOptions: {
