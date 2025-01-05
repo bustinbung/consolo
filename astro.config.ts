c4cf6842f7146fd290f515296d76eb5d2b092f68
Justin Jung <justin@bustinbung.com>
2025-01-04 22:28:13 -0500

    Revert "feat: add markdoc integration"
    This reverts commit 96f00e708413e11840874b88e3275b4db76eebd2.
    
    MDX likely better for this usecase. Markdoc doesn't support GFM footnote
    syntax.

--- astro.config.ts
+++ astro.config.ts
@@ -1,26 +1,20 @@
 import { defineConfig } from 'astro/config';
 
-import markdoc from '@astrojs/markdoc';
-
 // https://astro.build/config
 export default defineConfig({
-  site: "https://bustinbung.com",
-
-  markdown: {
-      shikiConfig: {
-          theme: 'catppuccin-frappe',
-      }
-  },
-
-  vite: {
-      css: {
-          preprocessorOptions: {
-              scss: {
-                  api: 'modern-compiler',
-              }
-          }
-      }
-  },
-
-  integrations: [markdoc()]
+    site: "https://bustinbung.com",
+    markdown: {
+        shikiConfig: {
+            theme: 'catppuccin-frappe',
+        }
+    },
+    vite: {
+        css: {
+            preprocessorOptions: {
+                scss: {
+                    api: 'modern-compiler',
+                }
+            }
+        }
+    }
 });
