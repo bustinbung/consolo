48d844cca16d5d56a0e4fb1b27a6b02813ac8a34
Justin Jung <justin@bustinbung.com>
2025-01-04 22:30:26 -0500

    feat: add mdx integration

--- astro.config.ts
+++ astro.config.ts
@@ -1,20 +1,26 @@
 import { defineConfig } from 'astro/config';
 
+import mdx from '@astrojs/mdx';
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://bustinbung.com",
-    markdown: {
-        shikiConfig: {
-            theme: 'catppuccin-frappe',
-        }
-    },
-    vite: {
-        css: {
-            preprocessorOptions: {
-                scss: {
-                    api: 'modern-compiler',
-                }
-            }
-        }
-    }
+  site: "https://bustinbung.com",
+
+  markdown: {
+      shikiConfig: {
+          theme: 'catppuccin-frappe',
+      }
+  },
+
+  vite: {
+      css: {
+          preprocessorOptions: {
+              scss: {
+                  api: 'modern-compiler',
+              }
+          }
+      }
+  },
+
+  integrations: [mdx()]
 });
